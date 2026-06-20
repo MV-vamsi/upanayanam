7c3c7b116aea478590c79b30cc86cfad9ee370a4
MV-vamsi <medepallivamsi123@gmail.com>
2026-06-20 23:41:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -34,13 +34,27 @@ function tick(){
     const seconds =
     Math.floor((diff/1000)%60);
 
-    document.getElementById('countdown').innerHTML =
-    `
-    <div>${days} Days</div>
-    <div>${hours} Hours</div>
-    <div>${minutes} Minutes</div>
-    <div>${seconds} Seconds</div>
-    `;
+   document.getElementById('countdown').innerHTML = `
+<div class="time-box">
+    <div class="time-number">${days}</div>
+    <div class="time-label">Days</div>
+</div>
+
+<div class="time-box">
+    <div class="time-number">${hours}</div>
+    <div class="time-label">Hours</div>
+</div>
+
+<div class="time-box">
+    <div class="time-number">${minutes}</div>
+    <div class="time-label">Minutes</div>
+</div>
+
+<div class="time-box">
+    <div class="time-number">${seconds}</div>
+    <div class="time-label">Seconds</div>
+</div>
+`;
 }
 
 tick();
